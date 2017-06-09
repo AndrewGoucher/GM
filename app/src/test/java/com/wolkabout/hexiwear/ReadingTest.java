d23a9c4b39d272b9d1f652a2c318b1ef47e274e4
Ryan <rcola45@hotmail.com>
2017-06-09 17:32:49 -0300

    Added Junit to build.gradle and added a ReadingTest in the new test folder

---
+++ app/src/test/java/com/wolkabout/hexiwear/ReadingTest.java
@@ -0,0 +1,26 @@
+package com.wolkabout.hexiwear;
+
+import android.content.Context;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+public class ReadingTest {
+    @Test
+    public void readTemp_Test(){
+        //Test for reading the temperature from hexiwear
+    }
+
+    @Test
+    public void readLight_Test(){
+        //Test for reading the light from hexiwear
+    }
+
+    @Test
+    public void readHumidity_Test(){
+        //Test for reading the humidity from hexiwear
+    }
+}
